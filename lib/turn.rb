cae2b5f1d6ee897c22c20ada5549df5e6a412e70
Eric Hasselbring <ehasselbring@gmail.com>
2016-09-15 15:31:41 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,47 @@
+def valid_move?(board, index)
+    if !position_taken?(board,index)
+        if board[index] == " X " || board[index] == "X" || board[index] == " O " || board[index] == "O"
+            return false
+        elsif index >= 0 && index <= 8
+            return true
+        end
+    else
+        return false
+    end
+end
+
+def position_taken?(board, index)
+    if  board[index] == "   " || board[index] == " " || board[index] == "" || board[index] == nil
+            return false
+        else
+            return true
+        end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  selection = gets.strip
+  index = input_to_index(selection)
+  if valid_move?(board, index)
+      move(board, index)
+      display_board(board)
+  else
+      turn(board)
+  end
+end
