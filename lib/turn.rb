a094b300a56da79cf3f268df13f02509dd38c35b
Cedric David <cedric.david@btopenworld.com>
2016-11-10 23:46:13 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,8 +18,8 @@ end
 
 
 
-def input_to_index(input)
-  return  (input.to_i)-1
+def input_to_index(user_input)
+  return  (user_input.to_i)-1
 end
 
 def move(board, index, value ="X")
@@ -27,13 +27,13 @@ def move(board, index, value ="X")
 end
 
 def turn(board)
-  loop do
     puts "Please enter 1-9:"
-    input = gets.chop
-    if valid_move?(board,input_to_index(input))
-      move(board, input_to_index(input), value = "X")
+    input = gets.strip
+    index =  input_to_index(input)
+    if valid_move?(board, index)
+      move(board, index)
+      display_board(board)
     else
-      puts "Please enter 1-9:"
+      turn(board)
     end
-  end
 end
