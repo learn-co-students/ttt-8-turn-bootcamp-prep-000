e1b855a59f4dc13dd389977652c2e98138f1fe71
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2016-11-22 02:47:46 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,18 +1,10 @@
 def valid_move?(board, ind)
-  if ind.between?(0, 8) && !position_taken?(board, ind)
-    true
-  else
-    false
-  end
+  ind.between?(0, 8) && !position_taken?(board, ind)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, ind)
-  if board[ind] == "" || board[ind] == " " || board[ind] == nil
-    false
-  else
-    true
-  end
+  !(board[ind] == "" || board[ind] == " " || board[ind] == nil)
 end
 
 def input_to_index(num)
@@ -34,14 +26,12 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-
-  loop do
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
-    break
+    display_board(board)
+  else
+    turn(board)
   end
-  end
-  display_board(board)
 end
