2ad871a8835488084ceae6220c19d4c5facb32b8
Duncan Maina <duncandevs@gmail.com>
2016-12-17 22:23:46 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,48 @@
+# mave one turn on tic tac toe
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board,index)
+    move(board,index)
+    display_board(board)
+  else 
+    turn(board)
+  end
+end
+
+# display the current board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(input)
+  index = input.to_i - 1
+end
+def move(board,index,char='X')
+  board[index] = char
+end
+
+# code your #valid_move? method here
+def valid_move?(board,index)
+  if index.between?(0,9) && !position_taken?(board,index)
+    true
+  else
+    false
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+end
+
