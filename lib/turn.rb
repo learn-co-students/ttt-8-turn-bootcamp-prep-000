adba4b33748bbd72cf9528568a7f31210c560fee
Malki Davis <md01@me.com>
2017-01-21 22:21:49 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your #valid_move? method here
+def valid_move? (board, index)
+  if index.between?(0, 8)
+    if position_taken?(board, index) == false
+      true
+    else position_taken?(board, index) == true
+      false
+    end
+  else
+    false
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken? (board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+
+end
