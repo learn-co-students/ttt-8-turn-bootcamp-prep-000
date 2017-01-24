86b7f49cf6f586fcb0faae0fb089291b6fabf319
John Cibula <johnjcibula@gmail.com>
2017-01-24 23:41:54 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,42 @@
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+puts "Where would you like to move? Please pick a number 1-9."
+user_input = gets.strip
+
+index = 0
+def input_to_index(user_input)
+   index = user_input.to_i
+   index -= 1
+   return index
+  end
+
+def move(board, index, character = "X")
+board[index] = character
+end
+
+def position_taken?(board,index)
+  if board[index] == "" || board[index] == " "
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board,index)
+  if position_taken? == false && index.between?(
+    0,board.length
+    )
+    return true
+  else
+    return false
+  end
+end
