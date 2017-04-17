367a33fa7edf22c69336b3d9f597182cbb94a595
Kyle Atmore <atmorek@gmail.com>
2017-04-17 19:49:55 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,51 @@
+# Define display_board that accepts a board and prints
+# out the current state.
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+
+# code your #valid_move? method here
+def valid_move?(board, index)
+  if (index.between?(0, 8) && !position_taken?(board, index))
+    return true
+  else
+    false
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    return false
+  else
+    return true
+  end
+end
+
+def move(board, index, player = "X")
+  board[index] = player
+end
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  until valid_move?(board, index)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+  end
+  move(board, index)
+  display_board(board)
+end
