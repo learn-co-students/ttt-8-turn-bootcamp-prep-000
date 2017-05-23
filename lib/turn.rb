65090636cb559f214c95609d1a783beb40c97b00
Ginger Wilson <connor.wilson221@gmail.com>
2017-05-23 18:42:31 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,19 +1,10 @@
 def valid_move?(board, index)
-  if board[index] == nil || position_taken?(board,index) == true
-    return false
-  elsif position_taken?(board, index) == false && board[index].between?(0,8)
-    return true
-  elsif board[index].between?(0, 8) == false
-    return false
-  end
+# Ruby returns the value of the last thing it reads. There for this conditional statement is already returning true or false. Since it is just checking that both position taken is false and index is truly between (0,8), we can write this conditional statement on one line of code.
+!position_taken?(board, index)  && index.between?(0,8)
 end
 
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    return false
-  elsif board[index] == "X" || board[index] == "O"
-    return true
-  end
+  board[index] == "X" || board[index] == "O"
 end
 
 def display_board(board)
@@ -37,5 +28,14 @@ def update_array_at_with(array, index, value)
   return array
 end
 
-def turn
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input= gets.strip
+  index = input_to_index(user_input)
+  if  valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
