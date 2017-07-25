5bea034b3da0a2e0201aeab292734b98040e8983
Christina Pan <christina.pl.pan@gmail.com>
2017-07-25 21:10:49 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,43 +1,32 @@
 #display_board method to show the current state of board
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} \n-----------"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
-  input = input.to_i - 1
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
 
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    false
-  else
-    true
-  end
+  !(board[index] == " ")
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, index)
-    true
-  else
-    false
-  end
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
-def move(board, index, character = "X")
-  board[index] = character
-  return board
+def move(board, index, char = "X")
+  board[index] = char
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  user_input = input_to_index(user_input)
-  if valid_move?(board, user_input)
-    move(board, user_input)
-    display_board(board)
-  else
-    turn(board)
-  end
+   puts "Please enter 1-9:"
+   input = gets.strip
+   input = input_to_index(input)
+   (valid_move?(board, input)) ? move(board, input):turn(board)
+   display_board(board)
 end
