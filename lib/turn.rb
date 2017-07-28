a8a213ce822af5a10e1bfb32f8a8f84611c7d109
Joey Cho <cjy12182000@naver.com>
2017-07-28 19:00:10 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,53 @@
+def valid_move?(board, index)
+  if(index.between?(0, 8))
+    !position_taken?(board,index)
+  else
+    false
+  end
+end
+
+def position_taken?(board,index)
+
+  if((board[index]==" ") || (board[index]=="") || (board[index]==nil))
+    false
+  else
+    true
+  end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i-1
+end
+
+def move(array, index, value="X")
+  array[index]=value
+=begin  if(value =="X")
+    $value = "O"
+  else
+    $value = 'X'
+  end
+=end
+end
+
+def turn(board)
+
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+
+  if valid_move?(board, index)
+    move(board,index)
+    display_board(board)
+  else
+      turn(board)
+  end
+
+end
