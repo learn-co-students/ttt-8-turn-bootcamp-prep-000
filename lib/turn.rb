cda82b8bb1f551d8092a45f9866a4c3a14e3de61
Derek Kwok <dkwok94@gmail.com>
2017-08-31 00:45:04 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,35 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def valid_move?(board, index)
+  if ((index<=8) && (index>=0) && (board[index] == " "))
+    true
+  else
+    false
+  end
+end
+
+def move(board, index, token = "X")
+  board[index] = token
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if !valid_move?(board,index)
+    turn(board)
+  else
+    move(board, index, token = "X")
+    display_board(board)
+  end
+end
