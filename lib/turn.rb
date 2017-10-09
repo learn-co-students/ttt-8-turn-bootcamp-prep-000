ce77ec2501ad54cdf72e8ed6358aef144808db4b
James Royer <walkingalchemy@gmail.com>
2017-10-09 03:47:23 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,25 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, input, char = "X")
+  board[input] = char
+end
+
+def valid_move?(board, index)
+  (index.between?(0,8) && !position_taken?(board, index)) ?
+  true : false
+end
+
+def position_taken?(board, position)
+  (board[position] == "X" || board[position] == "O") ?
+  true : false
+end
