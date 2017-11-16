4ec07164049dad789b876ea5d40f03199314cd82
PJ Frias <peejfrias@gmail.com>
2017-11-16 02:56:28 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,11 @@
+/*
+ask for input
+get input
+convert input to index
+if index is valid
+  make the move for index
+  show the board
+else
+  ask for input again until you get a valid input
+end
+*/
