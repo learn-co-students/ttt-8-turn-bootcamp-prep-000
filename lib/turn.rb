2c4dec74594c0b36744fbf4585416b6c783af0d3
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2017-12-12 20:09:42 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,8 +38,8 @@ def turn(board)
   index = input_to_index(num)
   if valid_move?(board, index)
     move(board, index)
+    display_board(board)
   else
     turn(board)
   end
-  display_board(board)
 end
