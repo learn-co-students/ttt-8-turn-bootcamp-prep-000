317b1598d16134623280f77d118a4d3f084a8f38
Lidian Vilazio <lidianvilazio@hotmail.com>
2018-01-23 00:23:31 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,12 +11,20 @@ def input_to_index(input)
 end
 
 def valid_move?(board, index)
-  if index >= 0 || index <=8
+  if index >= 0 && index <=8
     if !position_taken?(board, index)
       true
     end
   end
 
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def move(board, index, player = "X")
+  board[index] = player
 end
 
 def turn(board)
@@ -29,12 +37,4 @@ def turn(board)
   else
     turn(board)
   end
-end
-
-def position_taken?(board, index)
-  board[index] != " "
-end
-
-def move(board, index, current_player = "X")
-  board[index] = current_player
 end
