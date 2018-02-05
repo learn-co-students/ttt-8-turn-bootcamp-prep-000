101ca3ea947ccbbd3c80ef56d0032f715776cc54
Felice Forby <feliceforby@gmail.com>
2018-02-05 23:13:43 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,18 +28,9 @@ def move(board, position, char="X")
 end
 
 def valid_move?(board, index)
-  if !index.between?(0,8) || position_taken?(board, index)
-    false
-  else
-    true
-  end
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    false
-  elsif board[index] == "X" || board[index] == "O"
-    true
-  end
+  board[index] != " " && board[index] != "" && board[index] != nil
 end
-
