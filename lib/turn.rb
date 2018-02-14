50949570de2ae92ec3a9fa5e5adf877a4f69d171
Aspen James <aspenjames@tqca.org>
2018-02-14 04:30:46 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,20 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+end
+
+def valid_move?(board,index)
+  if index > board.length
+    return false
+  elsif board[index] == "" || board[index] == " " || board[index] == nil
+    return true
+  else
+    return false
+end
