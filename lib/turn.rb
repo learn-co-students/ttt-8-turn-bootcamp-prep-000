01c2fbf2805cbfa9224fcabab29978d8c7f6e8b6
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-20 20:29:19 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,47 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def turn(board, user_input)
+  puts "Please enter 1-9:"
+  if index.between?(0, 8)
+    true
+  else 
+    puts "Please enter 1-9:"
+  end
+end
+
+def input_to_index(user_input)
+  return user_input.to_i - 1
+end
+
+def valid_move?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+   false
+   elsif board[index] == "X" || board[index] == "O"
+   true
+  end
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+end
+
+
+
+
+
+
+
+
+
+
+
