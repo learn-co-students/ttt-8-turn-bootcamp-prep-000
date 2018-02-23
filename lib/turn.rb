7b1fba6d747a7b6fb6a141730ba1297c4bc79bed
Betsy Sallee <ensallee@gmail.com>
2018-02-23 17:34:41 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,8 +35,10 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input=gets.strip
   index=input_to_index(user_input)
-  while valid_move?(board, index)
-    move(board, index, character)
-  end
+  if valid_move?(board, index)
+  move(board, index)
+  display_board(board)
+  else
   turn(board)
+end
 end
