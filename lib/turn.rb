2a5fea3d4e485072ffe33043aaa75f32cdc4c6bf
Shawn Michael Larkin <shawn.michael.larkin@gmail.com>
2018-03-21 22:44:07 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,16 +6,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-
 def input_to_index(input)
   input.to_i - 1
 end
 
-
 def valid_move?(board, index)
   index.between?(0, 8) && !position_taken?(board, index)
 end
-
 
 def position_taken?(board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil)
@@ -25,12 +22,9 @@ def position_taken?(board, index)
   end
 end
 
-
-
 def move(board, index, player = "X")
   board[index] = player
 end
-
 
 def turn(board)
   puts "Please enter 1-9:"
