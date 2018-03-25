85d2ee9c99b2745e189e9e83caac6a9f730a5fd7
Nazibul Haque <elegant.nazib@gmail.com>
2018-03-25 11:07:17 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,4 @@
+### display_board
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,30 +7,33 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+### input_to_index
 def input_to_index(ui)
   ui.to_i - 1;
 end
+
+### move
 def move(board, index, player = 'X')
   board[index] = player
 end
+
 ###Valid_move?
 def valid_move?(board, index)
   a = board[index]
   filled = a == "X" || a == "O"
   index.between?(0,8) && !filled
 end
+
+### turn
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   input = input_to_index(user_input)
   vmove = valid_move?(board, input)
   if (vmove == false)
-    user_input = gets.strip
-  else
-  move(board, input)
-end
-display_board(board)
-
-
+      user_input = gets.strip
+    else
+      move(board, input)
   end
+      display_board(board)
+end
