5d3deeedc98a393ffc85064b6f27db64faf8bf25
Luke Worley <luke.worley@colorado.edu>
2018-05-21 18:17:44 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,42 @@
+def display_board(board)
+   
+  (0..6).step(3) do |n|
+    if (n<6)
+      puts " #{board[n]} | #{board[n+1]} | #{board[n+2]} "
+      puts "-----------"
+    else
+      puts " #{board[n]} | #{board[n+1]} | #{board[n+2]} "
+    end
+    
+  end
+end
+
+def input_to_index(input)
+  return input.to_i - 1
+end
+
+def position_taken?(board,index)
+  if board[index]=="" || board[index]==" " || board[index]==nil
+    return false
+  else
+    return true
+  end
+end
+    
+
+def valid_move?(board,index)
+  if !position_taken?(board,index) && index.between?(0,8)
+    return true
+  else
+    return false
+  end
+end
+
+def move(board,index,char="X")
+  board[index]=char
+end
+
+  
+#board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+#display_board(board)
+input_to_index("2")
