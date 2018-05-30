1aa1ee57a68ff0f8cf9cee21e53cabdedc5f1042
Amanda Kielley <eatingfoil@gmail.com>
2018-05-30 07:09:40 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,7 +31,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  # recursion passes the tests, but at first when I start the CLI myself and enter an invalid input one or more times followed by something valid, the board is printed multiple times. I assume this has something to do with working down the call stack. I realized this could be fixed by switching the order of validation, but this possible mistake isn't made clear in the lesson and requires a good deal more external knowledge. Maybe don't bring up recursion without expaining the call stack.
+  # recursion passes the tests, but at first when I started the CLI myself and entered an invalid input one or more times followed by something valid, the board was printed multiple times. I assume this has something to do with working down the call stack. I realized this could be fixed by switching the order of validation, but this possible mistake isn't made clear in the lesson and requires a good deal more external knowledge. Maybe don't bring up recursion without expaining the call stack, or otherwise just explicitly say, "you have to put the recursive method call at the end because, uh, reasons."
   if valid_move?(board, index)
     move(board, index)
     display_board(board)
