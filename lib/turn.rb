9e9fa4bc9ab8e65a05a1b91fd8d969f6ea18f97f
Amanda Kielley <eatingfoil@gmail.com>
2018-05-30 07:21:55 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,7 +31,8 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  # recursion passes the tests, but at first when I started the CLI myself and entered an invalid input one or more times followed by something valid, the board was printed multiple times. I realized this was because i was forgetting that recursively calling a function doesn't just terminate its current call stack instance without carrying out the following code (that in my case wasn't inside an "else" statement but just within the main block after the "if invalid, recurse" statement), but this possible mistake isn't made clear in the lesson and requires external knowledge. Maybe don't say to use recursion without expaining the call stack, or otherwise just explicitly say what you can and can't do.
+  # recursion passes the tests in general, but at first when I started the CLI myself and entered an invalid input one or more times followed by something valid, the board was printed multiple times. I realized this was because i was forgetting that recursively calling a function doesn't just terminate its current call stack instance without carrying out the following code (that in my case wasn't inside an "else" statement but just within the main method block after the "if invalid, recurse" statement), but this possible mistake isn't made clear in the lesson and requires external knowledge. Maybe don't say to use recursion without expaining the call stack, or otherwise just explicitly say what you can and can't do.
+  
   # here's the dumb thing I did at first:
   
 #  if !valid_move?(board, index)
