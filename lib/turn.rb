c8f7966184ff828094359292d524a3c6788d8b02
Amanda Kielley <eatingfoil@gmail.com>
2018-05-30 06:58:39 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,43 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1 
+end
+
+def position_taken?(board, i)
+  if board[i] == " " || board[i] == "" || board[i] == nil 
+    true 
+  end
+end
+
+def valid_move?(board, i)
+  if i.between?(0, 8) && position_taken?(board, i)
+    true 
+  end 
+end
+
+
+def move(board, i, token = "X")
+  board[i] = token 
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  #recursion passes the tests, but when I start the CLI myself and enter an invalid input one or more times followed by something valid, the board is printed multiple times. I assume this has something to do with working down the stack.
+  until valid_move?(board, index)
+    puts "Invalid input; please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+  end
+  move(board, index)
+  display_board(board)
+end 
+
