99d2936554969b62b9b1d69384854677048ee12f
Christabel Dapaa-Addo <cdapaaaddo@gmail.com>
2018-06-19 02:31:26 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,8 +18,8 @@ def valid_move?(board, index)
   (position_taken?(board, index) == false && index.between?(0, 8)) ? true : false
 end
 
-def move(board, index, character = "X")
-  board[index] = character
+def move(board, index, player = "X")
+  board[index] = player
   puts "#{display_board(board)}"
 end
 
