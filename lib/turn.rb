8a14f546da1901a7480753ae719569527b9b4277
Danny O'B <dannyk85.dob@gmail.com>
2018-06-20 15:12:26 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,6 +22,7 @@ def valid_move?(board_arr, index)
       return true
     end
   end
+  puts "invalid"
   return false
 end
 
@@ -32,15 +33,13 @@ end
 
 # the turn method
 def turn(board_arr)
-  display_board(board_arr)
   puts "Please enter 1-9:"
   user_input = input_to_index(gets.strip())
-  
   if valid_move?(board_arr, user_input)
     move(board_arr, user_input)
     display_board(board_arr)
   else
     puts "invalid"
+    turn(board_arr)
   end
-  
 end
