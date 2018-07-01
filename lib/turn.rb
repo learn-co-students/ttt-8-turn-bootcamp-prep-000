eabbfa54fcd0da47f996f1eeefd26fa358caec6f
gmat191@gmail.com <gmat191@gmail.com>
2018-07-01 00:29:03 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,4 +27,10 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, character = "X")
+    display_board(board)
+  else
+    turn(board)
+  end
 end
