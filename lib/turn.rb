2b6fbf52056c7efd0b566105b91f6cf38769d7f7
joseph.chesir@mail.yu.edu <joseph.chesir@mail.yu.edu>
2018-08-09 17:59:15 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,32 @@
 def display_board(board)
-  puts board 
-  end
+
+
+
+puts " #{board[0]} | #{board[1]} | #{board[2]} " 
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "  
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} " 
+
+
+end 
+
+def input_to_index(user_input)
+  user_input.to_i - 1 
+  
+ 
+
+end 
+
+def valid_move?(board,index)
+
+if index.between?(0,8) && !position_taken?(board, index)
+
+
+
+end 
+end 
+
+def position_taken?(array,index)
+  array[index] != " "
+end
