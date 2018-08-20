bbee65610290d46e7184ddfc6ca5e6ff66d86f9c
Pearl Zeng <zhuzeng0418@gmail.com>
2018-08-20 21:27:51 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,15 +12,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.to_i.between?(0, 8) == true 
-    if position_taken?(board, index) == true
-      false
-    else
-      true
-    end
-  else
-    false
-  end 
+  if index.to_i.between?(0, 8) || !position_taken?(board, index)
 end
 
 def input_to_index(input)
