12bd09b5141715174980a11114f58290946858e2
Austin  Burke <aburk3@gmail.com>
2018-09-10 18:03:32 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,6 +10,30 @@ def input_to_index(user_input)
 intInput = "#{user_input}".to_i - 1
 end
 
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board,index)
+end
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def position_taken?(board, index)
+  if (board[index] != " " && board[index] != "" && board[index] != nil)
+    return true
+  else 
+    return false
+  end
+end
 
 def move(boardArray, index, int_input= "X")
     boardArray[index] = int_input
