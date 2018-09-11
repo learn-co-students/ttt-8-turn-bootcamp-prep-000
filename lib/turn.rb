4e213c0204f494e3fbf1bfb8c8760c95931f0ff7
viqi zevic <vicky.tanzil@gmail.com>
2018-09-11 21:27:38 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,7 @@ def display_board(board)
   3.times do |i|
     j = i * 3
     puts " #{board[j]} | #{board[j+1]} | #{board[j+2]} "
-    if i < 3
+    if i < 2
       puts "-----------"
     end
   end
