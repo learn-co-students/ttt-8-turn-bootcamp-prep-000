6ea05b77b1519c247a1cb778d9af0eb6609d367a
naizal Edy <pritiadhikari@gmail.com>
2018-10-15 00:39:29 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,6 +8,14 @@ def display_board(board)
   
 end
 
+def input_to_index(input_to_index)
+  
+  input_to_index = input_to_index.to_i - 1
+  
+  return input_to_index
+  
+end
+
 
 def position_taken?(board,index)
   
@@ -24,6 +32,14 @@ end
 end
 
 
+def move(board, index, value = "X")
+  
+  if(index > -1 && index < 9)
+    board[index] = value;
+  end
+  
+  return board
+end
 
 def valid_move?(board, index)
   
@@ -42,6 +58,19 @@ if index.between?(0, 8) && position_taken?(board, index) == false
 
 end
 
+
+
+
 def turn(board)
-  puts "Please enter 1-9:"
+  isValid = false
+  while isValid == false
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    isValid = valid_move?(board, index)
+    if isValid
+      move(board, index)
+      display_board(board)
+    end
+  end
 end
