9460077bacfe25526a6bc658f289d4a27cb99220
Rishi Badiani <rishib9000@hotmail.com>
2019-02-05 23:39:35 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,7 @@ end
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
-  else board[index] == "X" || board[index] == "O"
+  else
     true
   end
 end
