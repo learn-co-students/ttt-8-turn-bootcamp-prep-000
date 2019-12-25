5599a8555679162cd89ec6bfdab6734ab474e7fa
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-25 18:29:12 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,24 +1,16 @@
 
-#board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
 def turn(board)
-puts "Please enter 1-9:"
-#display_board(board)
-#turn(board)
-  9.times do
-      #puts "Please enter 1-9:"
+      puts "Please enter 1-9:"
       input = gets.strip
       index = input_to_index(input)
-      ##position_taken?(board, index)
-      while !valid_move?(board, index)
-        puts "invalid position or position busy"
-        puts "Please enter 1-9:"
-        input = gets.strip
-        index = input_to_index(input)
+      if valid_move?(board, index)
+         move(board, index)
+         display_board(board)
+      else 
+        until valid_move?(board, index)
+          puts "Please enter 1-9:"
+        end
       end
-      move(board, index)
-      display_board(board)
-  end
 end
 
 def display_board(board)
@@ -29,33 +21,33 @@ def display_board(board)
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
   
-  def input_to_index(input)
+def input_to_index(input)
     input = input.to_i
     input = input-1
   end
   
-  def valid_move?(board, index)
+def valid_move?(board, index)
   if (index.between?(0,8)) && (!position_taken?(board,index))
-    puts "ok"
+    #puts "ok"
     return true
   else 
-    puts "position is busy"
+   # puts "position is busy"
     return false 
   end 
 end 
 
 def position_taken?(board, index)
   if board[index]=="" || board[index]==" " || board[index]== nil 
-    puts "position is free"
+    #puts "position is free"
     return false 
   else 
-    puts "position is not free"
+    #puts "position is not free"
     return true 
   end 
 end
 
 def move(board, index, player_char="X")
     board[index] = player_char
-    puts "the board index #{index} is filled"
+    #puts "the board index #{index} is filled"
   end
 
