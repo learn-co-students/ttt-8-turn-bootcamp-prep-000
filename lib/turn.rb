ff00c8b744c71d9fde42a6056e3c693226d2b514
Milos Popovic <milos_popovic@outlook.com>
2020-05-18 02:22:41 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,11 +2,11 @@
 
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   def display_board(array)
-    puts  " #{array[0]} | #{array[1]} | #{array[2]}"
+    puts  "#{array[0]} | #{array[1]} | #{array[2]}"
     puts "------------"
-    puts  " #{array[3]} | #{array[4]} | #{array[5]}"
+    puts  "#{array[3]} | #{array[4]} | #{array[5]}"
     puts "------------"
-    puts  " #{array[6]} | #{array[7]} | #{array[8]}"
+    puts  "#{array[6]} | #{array[7]} | #{array[8]}"
   end
 
   def input_to_index(input)
@@ -36,8 +36,9 @@ def turn(array)
   index = input_to_index(input)
     if valid_move?(array, index)
       move(array, index, token = "X")
+      display_board(array)
     else
       turn(array)
     end
-  display_board(array)
+
 end
