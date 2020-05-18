449553f2a1282be0b3c77e1f391b999cc1c38217
Milos Popovic <milos_popovic@outlook.com>
2020-05-18 02:15:01 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,11 +2,11 @@
 
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   def display_board(array)
-    puts  "#{board[0]} | #{board[1]} | #{board[2]}"
+    puts  " #{array[0]} | #{array[1]} | #{array[2]}"
     puts "------------"
-    puts  "#{board[3]} | #{board[4]} | #{board[5]}"
+    puts  " #{array[3]} | #{array[4]} | #{array[5]}"
     puts "------------"
-    puts  "#{board[6]} | #{board[7]} | #{board[8]}"
+    puts  " #{array[6]} | #{array[7]} | #{array[8]}"
   end
 
   def input_to_index(input)
